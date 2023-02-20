cc70c29e60c6cfe49751aa47b7f0e350c07d35c2
Jorge Diaz <jorjorf@gmail.com>
2023-02-20 10:09:57 -0400

    Manejo de eventos

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,8 +2,17 @@ import React from "react";
 import './TodoSearch.css';
 
 function TodoSearch(){
+
+    const onSearchValueChange = (event) => {
+        console.log(event.target.value);
+    }
+
     return(
-        <input className="TodoSearch" placeholder="Ingresar" />
+        <input 
+            className="TodoSearch" 
+            placeholder="Ingresar" 
+            onChange={onSearchValueChange}
+        />
     );
 }
 export {TodoSearch};
