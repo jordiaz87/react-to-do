d56e37df233abab92cd311596d5533e238834551
Jorge Diaz <jorjorf@gmail.com>
2023-02-20 19:08:39 -0400

    Completando y eliminando TODOs

--- src/App.js
+++ src/App.js
@@ -30,6 +30,21 @@ function App() {
       return todoText.includes(searchText);
     });
   }
+
+  const completeTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  }
+
+  const deleteTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  }
+
   return (
 
     <React.Fragment>
@@ -50,6 +65,8 @@ function App() {
           key={todo.text} 
           text={todo.text}
           completed={todo.completed}
+          onComplete={() => completeTodo(todo.text)}
+          onDelete={() => deleteTodo(todo.text)}
           />
         ))}
       </TodoList>
