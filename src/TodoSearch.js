130ed0cb5c3abc658eb71ff57dfa582ac540b946
Jorge Diaz <jorjorf@gmail.com>
2023-02-20 11:24:53 -0400

    Manejo del estado

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -3,16 +3,21 @@ import './TodoSearch.css';
 
 function TodoSearch(){
 
+    const [searchValue, setSearchValue] = React.useState('');
+
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
+        setSearchValue(event.target.value);
     }
 
-    return(
+    return[
         <input 
             className="TodoSearch" 
             placeholder="Ingresar" 
+            value={searchValue}
             onChange={onSearchValueChange}
-        />
-    );
+        />,
+        <p>{searchValue}</p>
+    ];
 }
 export {TodoSearch};
